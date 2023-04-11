2e399cd8c35a0c1e35610a51a8c2f98e70b61948
Arif <93457990+Arif556@users.noreply.github.com>
2023-04-11 10:04:18 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//---------------Highlight Real-Time Berdasarkan Tanggal-----------------------//
+
 function highlightToday() {
   const table = document.getElementById("jadwal");
   const rows = table.getElementsByTagName("tr");
@@ -16,5 +18,58 @@ function highlightToday() {
   }
 }
 
-// Eksekusi kode JavaScript setiap 1 menit
+// Eksekusi kode JavaScript setiap 1 Detik //
 setInterval(highlightToday, 100);
+
+//---------------------------------------------------------------------------//
+
+
+
+
+
+//---------------Tombol Sticky-----------------------//
+
+function scrollToToday() {
+    const table = document.getElementById("jadwal");
+    const rows = table.getElementsByTagName("tr");
+    const today = new Date().getDate();
+    let todayRow = null;
+  
+    for (let i = 0; i < rows.length; i++) {
+      const cols = rows[i].getElementsByTagName("td");
+      if (cols.length > 0) {
+        const date = new Date(cols[2].textContent).getDate(); // ubah indeks kolom menjadi 2
+        if (date === today) {
+          todayRow = rows[i];
+          break;
+        }
+      }
+    }
+  
+    if (todayRow) {
+      todayRow.scrollIntoView();
+    }
+  }
+  
+  function handleStickyBtn() {
+    const stickyBtn = document.getElementById("sticky-btn");
+    const table = document.getElementById("jadwal");
+    const tableTop = table.getBoundingClientRect().top;
+  
+    window.addEventListener("scroll", () => {
+      const scrollPos = window.scrollY;
+      const tablePos = table.getBoundingClientRect().top;
+      const isTableVisible = (tablePos - tableTop) <= 0;
+  
+
+    });
+  
+    stickyBtn.addEventListener("click", scrollToToday);
+  }
+  
+  handleStickyBtn();
+  
+
+
+
+//--------------------------------------------------//
